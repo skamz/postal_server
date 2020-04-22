dbdb8181661f9f2ab4eed9afd84fe54d58c76c7d
Zaporozhets Konstantin <zaporozhetskv@gogetlinks.net>
2020-04-22 12:29:05 +0400

    test

--- api/controllers/messages_api_controller.rb
+++ api/controllers/messages_api_controller.rb
@@ -12,7 +12,7 @@ controller :messages do
     action do
       server = identity.server
       {
-              host_domain: request.headers['HOST'],
+              host_domain: request.headers,
               held_messages: server.held_messages,
               queued_messages: server.queue_size,
               bounce_rate: server.bounce_rate,
